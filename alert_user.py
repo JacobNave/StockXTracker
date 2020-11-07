8ce312ab2dfed19c925e4d2e75dcc2c91d2f6d22
RobertGuerrero <bubbaguerrero2k@gmail.com>
2020-11-06 18:02:01 -0800

    Added add_item function and commented out send_sms temporarily

--- alert_user.py
+++ alert_user.py
@@ -7,5 +7,11 @@ def send_sms(item_price, num_input):
                         from_="+12059460266",
                         body="Your tracked item is now $" + str(item_price))
 
+def add_item(item_name, item_price, condition):
+    file = open("prices.txt", "a")
+    file.write(item_name + "," + str(item_price) + "," + str(condition) + "\n")
+    file.close()
+
 if __name__ == "__main__":
-    send_sms(200, "+18056125625")
+    #send_sms(200, "+18056125625")
+    add_item("Yeezy Boost 350 V2 Zebra", 320, 220)
