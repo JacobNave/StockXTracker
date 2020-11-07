7474b9d7c0aad2b316d855582e5b6969154e3eb1
Jacob Nave <jacobcnave1@gmail.com>
2020-11-06 16:30:09 -0800

    Reformatted files

---
+++ get_info.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from urllib.request import Request
+import re
+
+def get_html(search):
+    url = 'https://stockx.com/search/sneakers?s='
+    url += '%20'.join(search.split())  # search url
+    # print(url)
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req).read()
+    return page.decode('utf-8')
+
+def get_products(page):
+    print(page)
+    products = re.findall('<div>', page)
+    return products
--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from urllib.request import urlopen
-from urllib.request import Request
-import re
-
-def get_html(search):
-    url = 'https://stockx.com/search/sneakers?s='
-    url += '%20'.join(search.split())  # search url
-    # print(url)
-
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    return page.decode('utf-8')
-
-def get_products(page):
-    print(page)
-    products = re.findall('<div>', page)
-    return products
+from get_info import get_html
+from get_info import get_products
 
 if __name__ == '__main__':
     shoe = input('What shoe would you like to search: ')
-
 
     print(get_products(get_html(shoe)))
