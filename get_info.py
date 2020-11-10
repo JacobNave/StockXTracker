7f4ff695890faae9f7b5303c925381795da15589
Jacob Nave <jacobcnave1@gmail.com>
2020-11-09 22:45:31 -0800

    Gets search results after dynamic content loads

--- get_info.py
+++ get_info.py
@@ -1,17 +1,34 @@
 from urllib.request import urlopen
 from urllib.request import Request
+from bs4 import BeautifulSoup
 import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def get_html(search):
     url = 'https://stockx.com/search/sneakers?s='
     url += '%20'.join(search.split())  # search url
     # print(url)
 
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    return page.decode('utf-8')
+    options  = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("disable-gpu")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+    driver = webdriver.Chrome(executable_path= r'C:\Users\Jacob\Documents\chromedriver_win32\chromedriver.exe',chrome_options=options)
+    driver.get(url)
+    #req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    #page = urlopen(req).read()
+
+    #return page.decode('utf-8')
+    #return driver.page_source
+    return driver.execute_script("return document.body.innerHTML")
 
 def get_products(page):
     print(page)
     products = re.findall('<div>', page)
     return products
+
+def get_text(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return soup.prettify()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from get_info import get_html
 from get_info import get_products
+from get_info import get_text
 
 if __name__ == '__main__':
     shoe = input('What shoe would you like to search: ')
 
-    print(get_products(get_html(shoe)))
+    print(get_html(shoe))
