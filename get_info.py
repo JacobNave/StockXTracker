5ddd916cfd49df092bae393db8b2b11e8e40e13c
Jacob Nave <jacobcnave1@gmail.com>
2020-11-13 17:59:01 -0800

    Main allows for search and selection now to get price

--- get_info.py
+++ get_info.py
@@ -1,21 +1,25 @@
-from urllib.request import urlopen
-from urllib.request import Request
 from bs4 import BeautifulSoup
-import re
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import time
 
+def get_driver(user_driver):
+    if user_driver != None:
+        return user_driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("disable-gpu")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+    driver = webdriver.Chrome(executable_path=r'C:\Users\Jacob\Documents\chromedriver_win32\chromedriver.exe',
+                              chrome_options=options)
+    return driver
 
-def get_html(search):
+
+def get_html(search, user_driver=None):
     url = 'https://stockx.com/search/sneakers?s='
     url += '%20'.join(search.split())  # search url
 
-    options  = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    options.add_argument("disable-gpu")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-    driver = webdriver.Chrome(executable_path= r'C:\Users\Jacob\Documents\chromedriver_win32\chromedriver.exe',chrome_options=options)
+    driver = get_driver(user_driver)
     driver.get(url)
     time.sleep(1)
     return driver.execute_script("return document.body.innerHTML")
@@ -34,3 +38,21 @@ def get_products(page):
 def get_text(html):
     soup = BeautifulSoup(html, "html.parser")
     return soup.prettify()
+
+def get_product_page(suffix, user_driver=None):
+    url = 'https://stockx.com' + suffix
+
+    driver = get_driver(user_driver)
+
+    driver.get(url)
+    return driver.execute_script("return document.body.innerHTML")
+
+def get_price(suffix, user_driver=None):
+    url = 'https://stockx.com' + suffix
+
+    driver = get_driver(user_driver)
+    driver.get(url)
+    prod_page = driver.execute_script("return document.body.innerHTML")
+    soup = BeautifulSoup(prod_page, 'html.parser')
+    price = soup.find("div", {"class": "en-us stat-value stat-small"})
+    return price.decode_contents()
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from get_info import get_html
 from get_info import get_products
 from get_info import get_text
+from get_info import get_product_page
+from  get_info import get_price
+from selenium import webdriver
 
 if __name__ == '__main__':
     shoe = input('What shoe would you like to search: ')
 
-    print(get_products(get_html(shoe)))
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("disable-gpu")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+    driver = webdriver.Chrome(executable_path=r'C:\Users\Jacob\Documents\chromedriver_win32\chromedriver.exe',
+                              options=options)
+    products = get_products(get_html(shoe, driver))
+    for i in range(len(products)):
+        print(str(i + 1) + ': ' + products[i])
+    selection = int(input('\nChoose your best match: '))
+
+    print(get_price(products[selection-1], driver))
